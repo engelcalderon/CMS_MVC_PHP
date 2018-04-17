fe19c3cd4f9c5ed98af11fe446550116958cb9cd
Engel's MacBook Pro <ENGEL@MacBook-Pro-de-Engel.local>
2018-04-17 13:10:45 -0600

    added content to the about page

--- views/pages/about.php
+++ views/pages/about.php
@@ -1,6 +1,15 @@
 <div class="page">
     <div class="static">
         <h3 class="title">About us</h3>
-        <p class="body">Something about us</p>
+        
+        <div class="body">
+            <h6>Proyecto Programación 4</h6>
+            <p>Pagina web que permita a usuarios crear una cuenta para tener acceso al panel de administración y postear artículos bajo su nombre de usuario.</p>
+            <p>Integrantes:</p>
+            <ul>
+                <li>Frederick Calderon</li>
+                <li>Rrouss Molina</li>
+            </ul>
+        </div>
     </div>
 </div>
